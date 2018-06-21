dbf95a12781d5e629a97100bab9fff77e576f18f
Radek Postołowicz <postolowicz@gmail.com>
2018-06-21 21:14:14 +0200

    Remove dependency on commons lang

--- src/main/java/rpost/anxb/Reflections.java
+++ src/main/java/rpost/anxb/Reflections.java
@@ -1,18 +1,21 @@
 package rpost.anxb;
 
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.commons.lang3.reflect.MethodUtils;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Reflections {
 
     public static <T> T construct(Class<T> clazz, Class<?>[] argTypes, Object[] args) {
         try {
             Constructor<T> constructor = clazz.getDeclaredConstructor(argTypes);
-            assert constructor != null;
+            if (constructor == null) {
+                throw new IllegalArgumentException(
+                    "There is no constructor with argument types " + Arrays.toString(argTypes) + " in class " + clazz
+                );
+            }
             constructor.setAccessible(true);
             return constructor.newInstance(args);
 
@@ -29,10 +32,16 @@ public class Reflections {
     }
 
     public static <T> T invokeMethod(Object target, String methodName, Class[] argTypes, Object[] args) {
+        Method method = findMethod(target.getClass(), methodName, argTypes);
+        if (method == null) {
+            throw new IllegalArgumentException(
+                "There is no method named " + methodName + " with argument types " + Arrays.toString(argTypes) +
+                    " in class " + target.getClass()
+            );
+        }
+        method.setAccessible(true);
         try {
-            return (T) MethodUtils.invokeMethod(target, true, methodName, args, argTypes);
-        } catch (NoSuchMethodException e) {
-            throwRuntime(e);
+            return (T) method.invoke(target, args);
         } catch (IllegalAccessException e) {
             throwRuntime(e);
         } catch (InvocationTargetException e) {
@@ -41,14 +50,40 @@ public class Reflections {
         throw new IllegalStateException("should not happen ;)");
     }
 
+    private static Method findMethod(Class clazz, String methodName, Class[] argTypes) {
+        for (Class c = clazz; c != null; c = c.getSuperclass()) {
+            for (Method method: c.getDeclaredMethods()) {
+                if (method.getName().equals(methodName) && Arrays.equals(method.getParameterTypes(), argTypes)) {
+                    return method;
+                }
+            }
+        }
+        return null;
+    }
+
     public static <T> T getFieldValue(Object target, String fieldName) {
-        Field field = FieldUtils.getField(target.getClass(), fieldName, true);
+        Field field = findField(target.getClass(), fieldName);
+        if (field == null) {
+            throw new IllegalArgumentException("There is no field named " + fieldName + " in class " + target.getClass());
+        }
+        field.setAccessible(true);
         try {
             return (T) field.get(target);
         } catch (IllegalAccessException e) {
             throwRuntime(e);
         }
         throw new IllegalStateException("should not happen ;)");
+    }
+
+    private static Field findField(Class clazz, String fieldName) {
+        for (Class c = clazz; c != null; c = c.getSuperclass()) {
+            for (Field field : c.getDeclaredFields()) {
+                if (field.getName().equals(fieldName)) {
+                    return field;
+                }
+            }
+        }
+        return null;
     }
 
     private static void throwRuntime(Throwable e) {
